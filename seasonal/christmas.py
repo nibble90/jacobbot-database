d7b94f421c8e9e68547af1a11c283580a8c69866
Ben Atkinson <nibble90@live.com>
2019-12-05 15:34:33 +0000

    Added Christmas countdown function

--- seasonal/christmas.py
+++ seasonal/christmas.py
@@ -1,1 +1,7 @@
 class Christmas:
+    def __init__(self, user_id, context):
+        self.uuid = user_id
+        self.context = context
+    def countdown(self):
+        self.context.bot.send_message(chat_id=self.uuid,
+                                      text="Return countdown here")
