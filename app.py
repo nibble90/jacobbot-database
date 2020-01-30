8d86b1c1a4a98f07f64312d3ae2e6a0bdabb23db
nibble90 <nibble90@live.com>
2020-01-30 23:10:48 +0000

    Website landing page

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def landing_page():
-    return render_template("index.html")
+    return render_template("landing.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080', debug=True)
---
+++ static/js/landing.js
@@ -0,0 +1,8 @@
+function adminpanel(){
+alert("Admin Panel Button Pressed");
+//window redirect
+}
+function signup(){
+alert("Signup Button Pressed");
+//window redirect
+}
