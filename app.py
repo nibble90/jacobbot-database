ea3bcd10c44d394d615c2fbaf1eb62a1901bb2ba
nibble90 <nibble90@live.com>
2020-02-24 21:47:00 +0000

    Updated app.py to allow for the robots.txt file to work

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route("/")
 def landing_page():
@@ -32,7 +32,9 @@ def signup_request():
         return data
     else:
         return "No thank you..."
-
+@app.route("/robots.txt")
+def robotstxt():
+    return render_template("robots.txt")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080', debug=True)
