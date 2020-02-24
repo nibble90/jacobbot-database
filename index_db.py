541b2b426dc861e9d013943baa5ac79085dc7a0a
nibble90 <nibble90@live.com>
2020-02-24 21:13:36 +0000

    Merged database files, checked; fixed os style for command identification

--- index_db.py
+++ index_db.py
@@ -3,37 +3,62 @@ from sys import argv
 class command_line:
     def __init__(self):
         self.arguments = argv
+
     def unix(self):
         pass
+
     def gnu(self):
         pass
+
     def windows(self):
         pass
+
     def identify(self):
         dash = 0
         slash = 0
-        for character in argv[1]:
+        for character in self.arguments[1]:
             if(character == "-"):
                 dash += 1
             elif(character == "/"):
                 slash += 1
-        if(dash = 1):
+        if(dash == 1):
             print("UNIX")
-        elif(dash = 2):
+            print(*self.arguments)
+        elif(dash == 2):
             print("GNU")
-        elif(slash = 1):
+            print(*self.arguments)
+        elif(slash == 1):
             print("WINDOWS")
+            print(*self.arguments)
         else:
             print("Error, found {} dashes and {} slashes".format(dash, slash))
+            print(*self.arguments)
+        self.strip()
+        print(*self.arguments)
+
+    def strip(self):
+        temp_argv = []
+        for element in self.arguments:
+            if(element[0] == "-" or element[0] == "/"):
+                if(element[1] == "-"):
+                    temp_argv.append(element[2:])
+                else:
+                    temp_argv.append(element[1:])
+            else:
+                temp_argv.append(element)
+        self.arguments = temp_argv
     
 class jb_db:
-    def __init__(self, ):
-        pass
-    def create():
-        connection = sqlite3.connect('jacobbot_database.db')
+    def __init__(self, database_filename):
+        self.db_name = database_filename
+
+    def create(self):
+        connection = sqlite3.connect(self.db_name)
         c = connection.cursor()
         c.execute('''CREATE TABLE users
-             (date text, trans text, symbol text, qty real, price real)''')
+             (uuid text, normal_user boolean, admin_user boolean)''')
+
 if __name__ == "__main__":
-    command_line.identify()
-    #database = jb_db()
+    #command = command_line()
+    #command.identify()
+    jb_db('jacobbot.db').create()
