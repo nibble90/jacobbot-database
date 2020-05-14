b0f82f39d6dd8d622e60e8cf2a32fae185b467bb
nibble90 <nibble90@live.com>
2020-05-14 18:02:55 +0000

    Tested more functions added in database_security.py

--- test.py
+++ test.py
@@ -5,14 +5,19 @@ from database_security import AccessDatabase
 
 class TestDatabaseSecurity(unittest.TestCase):
     def setUp(self):
-        self.address = "192.168.0.0"
-        self.AD = AccessDatabase(self.address)
+        self.AD = AccessDatabase()
+        self.AD.ip_address = "192.168.0.0"
+        self.AD.uuid = "123456"
     def test_reset(self):
         self.assertTrue(self.AD.reset_attempts())
     def test_add_attempt(self):
         self.assertTrue(self.AD.add_attempt())
     def test_attempts(self):
         self.assertEqual(self.AD.attempts(), 1)
+    def test_add_user(self):
+        self.assertFalse(self.AD.add_user())
+    def test_uuid_check(self):
+        self.assertTrue(self.AD.uuid_check())
 
 
 
