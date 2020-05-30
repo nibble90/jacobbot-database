425d929c59320dafe4fb221b50e602a87bb7ced2
nibble90 <nibble90@live.com>
2020-05-30 17:23:47 +0100

    Updated the TODO at the top of database_access_later.py

--- database_access_layer.py
+++ database_access_layer.py
@@ -3,7 +3,7 @@
 TODO:
     [X] Grab IP address from application (mainly flask)
     [Y] Store IP address in seperate database with number of tries
-    [X] If tries > 5 then block IP address
+    [Y] If tries > 5 then block IP address
     [X] Super-Admin must unblock IP address
     [X] If username / password combination is correct before tries is up then
     tries is reset to 0 and user logged in
